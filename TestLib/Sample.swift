4312b897d364c0351da43893549180b8f1c815bb
S.Itou <Shouko.Itou-N467@nid.co.jp>
2019-10-31 11:40:03 +0900

    Initial Commit

---
+++ TestLib/Sample.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public class Sample {
+
+    public init() {}
+
+    public var str: String = "サウナイキタイ"
+
+    public func getStr() -> String {
+        return self.str
+    }
+}
+
---
+++ TestLib/TestLib.h
@@ -0,0 +1,19 @@
+//
+//  TestLib.h
+//  TestLib
+//
+//  Created by itou on 2019/10/31.
+//  Copyright © 2019 itou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLib.
+FOUNDATION_EXPORT double TestLibVersionNumber;
+
+//! Project version string for TestLib.
+FOUNDATION_EXPORT const unsigned char TestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLib/PublicHeader.h>
+
+
